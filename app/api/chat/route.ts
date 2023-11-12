8f6c7642a2c6ea2485ff1a3879c3016fd8c5fc2d
VTS <vytautas.s@gmail.com>
2023-11-12 12:51:27 +0000

    switch to langchain-openai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,16 +2,46 @@ import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
+import { XataVectorSearch } from "langchain/vectorstores/xata";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { BaseClient } from "@xata.io/client";
+import { ChatOpenAI } from 'langchain/chat_models/openai'
+
+import { VectorDBQAChain } from "langchain/chains";
+import { OpenAI } from "langchain/llms/openai";
+
+import { PromptTemplate } from "langchain/prompts";
+import { StringOutputParser } from "langchain/schema/output_parser";
+import { RunnableSequence } from "langchain/schema/runnable";
+import { formatDocumentsAsString } from "langchain/util/document";
+
+// Just import getXataClient from the generated xata.ts instead.
+const getXataClient = () => {
+  if (!process.env.XATA_API_KEY) {
+    throw new Error("XATA_API_KEY not set");
+  }
+
+  if (!process.env.XATA_DB_URL) {
+    throw new Error("XATA_DB_URL not set");
+  }
+  const xata = new BaseClient({
+    databaseURL: process.env.XATA_DB_URL,
+    apiKey: process.env.XATA_API_KEY,
+    branch: process.env.XATA_BRANCH || "main",
+  });
+  return xata;
+};
+
+const client = getXataClient();
+const table = "vectors";
+const embeddings = new OpenAIEmbeddings();
+const vectorStore = new XataVectorSearch(embeddings, { client, table });
+
+
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
 
 export const runtime = 'edge'
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
   const json = await req.json()
@@ -23,78 +53,77 @@ export async function POST(req: Request) {
       status: 401
     })
   }
+
+  // const stream = OpenAIStream(res, {
+  //   async onCompletion(completion) {
+  //     const title = json.messages[0].content.substring(0, 100)
+  //     const id = json.id ?? nanoid()
+  //     const createdAt = Date.now()
+  //     const path = `/chat/${id}`
+  //     const payload = {
+  //       id,
+  //       title,
+  //       userId,
+  //       createdAt,
+  //       path,
+  //       messages: [
+  //         ...messages,
+  //         {
+  //           content: completion,
+  //           role: 'assistant'
+  //         }
+  //       ]
+  //     }
+  //     await kv.hmset(`chat:${id}`, payload)
+  //     await kv.zadd(`user:chat:${userId}`, {
+  //       score: createdAt,
+  //       member: `chat:${id}`
+  //     })
+  //   }
+  // })
+
+  const questionPrompt = PromptTemplate.fromTemplate(
+    `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Act as a Residency Lawyer Professional in Portugal, working with expats who are looking to relocate to Portugal. You are precise in your answers and give friendly explanations of the steps required to get Visa in Portugal also you give detailed requirements for each persons case. You must always ask 1 (one) and more follow-up question.
+  ----------
+  CONTEXT: {context}
+  ----------
+  CHAT HISTORY: {chatHistory}
+  ----------
+  QUESTION: {question}
+  ----------
+  Helpful Answer:`
+  );
   
-  if (messages.length >4) {
-    const summaryMessage = {
-      role: 'system',
-      content: "Answer last user message, summarize the conversation, thank the user and suggest to submit a request to one of our approved residency consultants for best-in-class service."
-    };
-    messages.push(summaryMessage);
-    // There are messages in the array
-    // Perform some action
-  } else {
-    // The array is empty
-    // Perform some other action
-  }
+  const retriever = vectorStore.asRetriever();
+  const model = new ChatOpenAI({
+    streaming: true,
+  });
+
+  const chain = RunnableSequence.from([
+    {
+      question: (input: { question: string; chatHistory?: string }) =>
+        input.question,
+      chatHistory: (input: { question: string; chatHistory?: string }) =>
+        input.chatHistory ?? "",
+      context: async (input: { question: string; chatHistory?: string }) => {
+        const relevantDocs = await retriever.getRelevantDocuments(input.question);
+        const serialized = formatDocumentsAsString(relevantDocs);
+        return serialized;
+      },
+    },
+    questionPrompt,
+    model,
+    new StringOutputParser(),
+  ]);
   
+  let lastMessage = messages[messages.length - 1].content;
+  console.log("lastMessage");
+  console.log(lastMessage);
 
-  if (previewToken) {
-    configuration.apiKey = previewToken
-  }
+  const stream2 = await chain.stream({
+    question: lastMessage,
+  });
 
-  // Define the system message
-  const systemMessage = {
-    role: 'system',
-    content: "Act as a Residency Lawyer Professional in Portugal, working with expats who are looking to relocate to Portugal. You are precise in your answers and give friendly explanations of the steps required to get Visa in Portugal also you give detailed requirements for each persons case. You do not discuss any details not related to residency. You must always ask 1 (one) and more follow-up question."
-  };
-
-  messages.unshift(systemMessage);
-
-  console.log(messages);
-
-
-  const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo-1106',
-    messages,
-    temperature: 0.1,
-    stream: true
-  })
-
-  console.log(messages);
-
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
-
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream2)
 }
-
-
-
-
-
